212800105db1b5c2c44e5e34ca8c70ba436a68fc
ERIC_BASKOVICH <eric.baskovich@gmail.com>
2024-09-13 08:43:44 -0600

    added nav to signup

--- front_end/src/pages/SignupPage.jsx
+++ front_end/src/pages/SignupPage.jsx
@@ -1,22 +1,23 @@
 //THE SIGNUP PAGE IS ONE OF TWO (ALONG WITH LOGIN.JSX) PAGES ON A PUBLIC ROUTE  .
-import React from 'react'
-import { useState } from "react"
-import { Link } from "react-router-dom"
-import { userRegistration } from '../utilities.jsx'
+import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { userRegistration } from "../utilities.jsx";
 
 function Signup() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const Navigate = useNavigate();
 
-
-//HANDLES FORM SUBMIT, PREVENTS FORM FROM REFRESHING PAGE BY DEFAULT. CALLS SIGN UP FUNCTION IN UTILITIES.JSX. USER MUST THEN NAVIGATE TO LOGIN PAGE TO LOGIN, FUTURE ITERATION WILL LOGIN UPON ACCOUNT CREATION.
+  //HANDLES FORM SUBMIT, PREVENTS FORM FROM REFRESHING PAGE BY DEFAULT. CALLS SIGN UP FUNCTION IN UTILITIES.JSX. USER MUST THEN NAVIGATE TO LOGIN PAGE TO LOGIN, FUTURE ITERATION WILL LOGIN UPON ACCOUNT CREATION.
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      const user = await userRegistration(email, password)
-      console.log("User registered:", user)
+      const user = await userRegistration(email, password);
+      console.log("User registered:", user);
+      Navigate("/login");
     } catch (error) {
-      console.error("Registration failed:", error)
+      console.error("Registration failed:", error);
     }
   };
 
@@ -37,7 +38,9 @@ function Signup() {
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form onSubmit={handleSubmit} className="space-y-6">
           <div>
-            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900">
               Email address
             </label>
             <div className="mt-2">
@@ -55,7 +58,9 @@ function Signup() {
 
           <div>
             <div className="flex items-center justify-between">
-              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900">
                 Password
               </label>
             </div>
@@ -75,23 +80,23 @@ function Signup() {
           <div>
             <button
               type="submit"
-              className="flex w-full justify-center rounded-md bg-blue-600/80 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
+              className="flex w-full justify-center rounded-md bg-blue-600/80 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
               Sign up
             </button>
           </div>
         </form>
       </div>
       <div className="mt-6 text-center">
-          <h4 className="text-lg font-medium text-gray-900">OR</h4>
-          {/*LINK TO NEAR IDENTICAL LOGIN IN PAGE THAT CALLS USER SIGN UP FUNCTION IN UTILITIES.JSX. THIS WILL BE MADE INTO ONE COMPONENT WITH TOGGLE STATE VICE A SEPARATE PAGE*/}
-          <Link to="/login" className="mt-2 block text-lg font-medium text-red-700 hover:text-red-800">
-            Login to your account
-          </Link>
-        </div>
+        <h4 className="text-lg font-medium text-gray-900">OR</h4>
+        {/*LINK TO NEAR IDENTICAL LOGIN IN PAGE THAT CALLS USER SIGN UP FUNCTION IN UTILITIES.JSX. THIS WILL BE MADE INTO ONE COMPONENT WITH TOGGLE STATE VICE A SEPARATE PAGE*/}
+        <Link
+          to="/login"
+          className="mt-2 block text-lg font-medium text-red-700 hover:text-red-800">
+          Login to your account
+        </Link>
       </div>
-    
-  )
+    </div>
+  );
 }
 
-export default Signup
+export default Signup;
